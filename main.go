7b8002339adcbccf852690697b8cfba4bf4ac04a
Iheanacho Ebere <eiheanacho52@gmail.com>
2023-03-08 20:44:52 +0100

    Feat:Added Chef Module features

--- main.go
+++ main.go
@@ -1,12 +1,123 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"net/http"
+	"time"
+)
+
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
+type Recipe struct {
+	Id           string    `json:"id"`
+	Name         string    `json:"name"`
+	Keywords     []string  `json:"keywords"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+	Chef         Chef      `json:"chef"`
+}
+
+type Chef struct {
+	Name                string `json:"name"`
+	Country             string `json:"country"`
+	Years_of_experience int    `json:"years_Of_Experience"`
+}
+
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("recipe-id")
+	index := -1
+
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].Id == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe deleted",
+	})
+}
+
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("recipe-id")
+
+	var recipe Recipe
+
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	index := -1
+
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].Id == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+
+	recipe.Id = id
+	recipes[index] = recipe
+
+	c.JSON(http.StatusOK, recipe)
+}
+
+func ListRecipesHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, recipes)
+}
+
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if recipe.Chef.Name == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Chef name is Required.",
+		})
+		return
+	}
+
+	recipe.Id = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello world"})
-	})
-
+	router.POST("/Create-recipes", NewRecipeHandler)
+	router.GET("/recipes", ListRecipesHandler)
+	router.PUT("recipes/:recipe-id", UpdateRecipeHandler)
+	router.DELETE("recipes/:recipe-id", DeleteRecipeHandler)
 	router.Run()
 }
